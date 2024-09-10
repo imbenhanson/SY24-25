669696ba286db0ac72a5be041990fee1b05d50c6
Ben Hanson <bphan178@learner.dvusd.org>
2024-09-10 09:12:36 -0700

    minesweeper
    added minesweeper project to gitkraken folder

---
+++ Minesweeper/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Form1 : Form
+    {
+        Button[] btnGrid = new Button[100];
+        tile[] tileGrid = new tile[100];
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 100; i++)
+            {
+                btnGrid[i] = (Button)Controls["button" + (i + 1)];
+                tileGrid[i]=new tile(btnGrid[i]);
+                btnGrid[i].BackColor = Color.Green;
+
+            }
+        }
+        private Button getButton(int r, int c)
+        {
+            return (Button)getButton(r, c);
+        }
+        private int getIndex(Button b)
+        {
+            string tmp = b.Name.Substring(6);
+            int retVal = 0;
+            int.TryParse(tmp, out retVal);
+            return retVal - 1;
+        }
+        private void button91_MouseDown(object sender, MouseEventArgs e)
+        {
+            Button b = sender as Button;
+            b.BackColor = Color.Red;
+        }
+    }
+}
